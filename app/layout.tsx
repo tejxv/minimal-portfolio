63098f869ef9a401482f2fbf2c37dab54cfae6a0
tejas <upadhyaytejas100@gmail.com>
2024-07-11 17:45:24 +0530

    add hotjar

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import Footer from "./components/footer"
 import Header from "./components/header"
 import { baseUrl } from "./sitemap"
 import EmblaCarousel from "./components/slider"
+import HotjarScript from "./components/HotjarScript"
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -47,6 +48,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className}>
+      <HotjarScript />
       <body className="antialiased max-w-full mx-4 lg:mx-auto">
         <Header />
         <EmblaCarousel />
