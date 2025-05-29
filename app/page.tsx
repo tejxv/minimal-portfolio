df8b7472f881d1276a1309dc80f64fe3599fc4d1
tejas <upadhyaytejas100@gmail.com>
2025-05-29 10:58:39 +0530

    name change

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { BlogPosts } from "app/components/posts"
 import Education from "./components/education"
 import Experience from "./components/experience"
 import Contact from "./components/contact"
+import SelectedWorks from "./components/SelectedWorks"
 
 export default function Page() {
   return (
@@ -11,15 +12,24 @@ export default function Page() {
       </h1> //might use for seo for names - tejas */}
       <h1 className="font-semibold text-2xl mb-8 tracking-tight">About me</h1>
       <p className="mb-4 text-neutral-500">
-        My name is Tejas Upadhyay. A product designer who loves to code, I'm
-        currently working at Blogpro, a startup that converts your Notion pages
-        into a well-designed blog.
+        I'm Tejas Upadhyay, a product designer who loves to code. I'm currently
+        working at{" "}
+        <a
+          href="https://superboard.xyz"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-neutral-800 hover:text-neutral-600 transition-colors"
+        >
+          Superboard.xyz
+        </a>
+        , a web3 startup.
         <br></br> My passion is creating experiences at the intersection of art,
         design, and accessibility.
       </p>
       <div className="my-8">
         <Experience />
       </div>
+      {/* <SelectedWorks /> */}
       {/* <div className="my-8">
         <Education />
       </div> */}
